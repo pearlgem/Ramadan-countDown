61e8657babf6918e078b02d82992af655cc3d3e1
Hyder Ali <160383412+pearlgem@users.noreply.github.com>
2025-02-15 22:20:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 alert("UI ko time nhi diya, Sorry");
+
 function updateCountdown() {
   let ramazanDate = new Date("Feb 28, 2025 00:00:00").getTime();
   let now = new Date().getTime();
-  let bakiTime = ramazanDate - now;
+  let timeLeft = ramazanDate - now;
 
-  let days = Math.floor(bakiTime / 86400000);
-  bakiTime = bakiTime % 86400000;
-  let hours = Math.floor(bakiTime / 3600000);
-  bakiTime = bakiTime % 3600000;
-  let minutes = Math.floor(bakiTime / 60000);
-  bakiTime = bakiTime % 60000;
-  let seconds = Math.floor(bakiTime / 1000);
+  let days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
+  timeLeft = timeLeft % (1000 * 60 * 60 * 24);
+
+  let hours = Math.floor(timeLeft / 1000 / 60 / 60);
+  timeLeft = timeLeft % (1000 * 60 * 60);
+
+  let minutes = Math.floor(timeLeft / 1000 / 60);
+  timeLeft = timeLeft % (1000 * 60);
+
+  let seconds = Math.floor(timeLeft / 1000);
+
   document.getElementById("days").textContent = days;
   document.getElementById("hours").textContent = hours;
   document.getElementById("minutes").textContent = minutes;
   document.getElementById("seconds").textContent = seconds;
 }
+
 setInterval(updateCountdown, 1000);
